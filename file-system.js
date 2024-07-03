ddee27e0a14866867fb0f1799e30fc2dcdfa51df
fymryn <fymrynight@gmail.com>
2024-07-03 15:25:26 +0600

    testing file-system module

---
+++ file-system.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const path = require('path')
+
+// fs.mkdirSync(path.resolve(__dirname, 'dir'))
+// fs.mkdirSync(path.resolve(__dirname, 'dir0', 'dir2', 'dir3'), {recursive: true})
+
+
+// console.log('start')
+
+// fs.mkdir(path.resolve(__dirname, 'dir'), (error) => {
+//     if(error) {
+//      return console.log(error)
+//     }
+
+//     console.log('папка создана!')
+// })
+
+// console.log('end')
+
+// fs.rmdir(path.resolve(__dirname, 'dir'), (error) => {
+//     if(error) {
+//         throw error;
+//     }
+// })
+
+// fs.writeFile(path.resolve(__dirname, 'test.txt'), 'qwerty123123 42 23', (error) => {
+//     if(error) {
+//         throw(error)
+//     }
+//     console.log('Файл записан.')
+// })
+
+// fs.appendFile(path.resolve(__dirname, 'test.txt'), ' + строка в конец!', (error) => {
+//     if(error) {
+//         throw(error)
+//     }
+//     console.log('В файл добавленны данные')
+// })
+
+// fs.writeFiles(path.resolve(__dirname, 'test.txt'), 'qwerty123123 42 23', (error) => {
+//     if(error) {
+//         throw(error)
+//     }
+//     fs.appendFile(path.resolve(__dirname, 'test.txt'), ' + строка в конец!', (error) => {
+//         if(error) {
+//         throw(error)
+//     }
+//     console.log('В файл добавленны данные')
+// })
+
+//     console.log('Файл записан.')
+// })
+
