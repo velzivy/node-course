be2f9bdf203cb7dc23ec13101e624c7348dcbb29
fymryn <fymrynight@gmail.com>
2024-07-05 04:39:24 +0600

    create Router class

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
 const http = require('http');
 
+const EventEmitter = require('events');
 const PORT = process.env.PORT || 5000;
+
+
+const emitter = new EventEmitter()
+
+class Router {
+    constructor() {
+        this.endpoints = {}
+    }
+
+    request(method = 'GET', path, handler) {
+        if(!this.endpoints[path]) {
+            this.endpoints[path] = {}
+        }
+
+        const endpoint = this.endpoints[path];
+
+        if(endpoint[method]) {
+            throw new Error(`[${method}] по адресу ${path} уже создан`)
+        }
+
+        endpoint[method] = handler
+        emitter.on(`[${path}]:[${method}]`, (req, res) => {
+            handler(req, res)
+        })
+    }
+
+    get(path, handler) {
+        this.request('GET', path, handler)
+    }
+    
+    post(path, handler) {
+        this.request('POST', path, handler)
+    }
+
+    put(path, handler) {
+        this.request('PUT', path, handler)
+    }
+
+    delete(path, handler) {
+        this.request('DELETE', path, handler)
+    }
+}
+
+const router = new Router()
+
+router.get('/users', (req, res) => {
+    res.end(`Вы отправили запрос на ${req.url}`)
+})
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-type': 'application/json'
+        'Content-type': 'text/html; charset=utf-8'
     })
 
-    if(req.url === '/users') {
-        return res.end(JSON.stringify([
-            {id: 1, name: 'Fymryn'}
-        ]))
-    }
+    
+    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
 
-    if(req.url === '/posts') {
-        return res.end('POSTS')
+    if(!emitted) { // возвращает boolean значение
+        res.end()
     }
-
-    res.end(req.url)
 })
+
 server.listen(PORT, () => {
     console.log(`Сервер запустился на пору ${PORT}`)
 })
