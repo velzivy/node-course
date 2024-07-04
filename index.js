ec043b98f9fe76e30fc33ad63a6a3c692a3f93be
fymryn <fymrynight@gmail.com>
2024-07-05 04:13:26 +0600

    create endpoints

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-type': 'text/html; charset=utf-8'
+        'Content-type': 'application/json'
     })
-    res.end('<h1>hello world!<button>Click me!</button></h1>')
+
+    if(req.url === '/users') {
+        return res.end(JSON.stringify([
+            {id: 1, name: 'Fymryn'}
+        ]))
+    }
+
+    if(req.url === '/posts') {
+        return res.end('POSTS')
+    }
+
+    res.end(req.url)
 })
 server.listen(PORT, () => {
     console.log(`Сервер запустился на пору ${PORT}`)
