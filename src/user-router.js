e505f00dde2968adaa0543572ae58b5ffeb649d6
fymryn <fymrynight@gmail.com>
2024-07-07 05:24:44 +0600

    fix: == in params

--- src/user-router.js
+++ src/user-router.js
@@ -12,7 +12,7 @@ router.get('/users', (req, res) => {
     console.log(req.params);
 
     if(req.params.id) {
-        return res.send(users.find(user => user.id === req.params.id))
+        return res.send(users.find(user => user.id == req.params.id))
     }
     res.send(users);
 })
